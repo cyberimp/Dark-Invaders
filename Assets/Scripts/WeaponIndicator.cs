09b1381c461156b8e22715ef237f9b023c98d638
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-11 14:58:10 +0300

    Current weapon indicator

---
+++ Assets/Scripts/WeaponIndicator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WeaponIndicator : MonoBehaviour {
+
+
+    private Image imageComponent;
+	// Use this for initialization
+	void Start () {
+        imageComponent = GetComponent<Image>();
+        UpdateImage();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void UpdateImage()
+    {
+        GameObject gun = GameObject.FindGameObjectWithTag("Player").GetComponent<ShipController>().Gun1;
+        imageComponent.sprite = gun.GetComponent<SpriteRenderer>().sprite;
+    }
+}
