8ececd1768975183daf28e33c2151c161146bcc9
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-11 15:14:44 +0300

    Trackinfo indicator added

---
+++ Assets/Scripts/MusicIndicator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MusicIndicator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SetMusic("xenobeat\n\tgoreshit");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void SetMusic(string trackinfo)
+    {
+        Text tx = GetComponent<Text>();
+        tx.text = "♫" + trackinfo;
+    }
+}
