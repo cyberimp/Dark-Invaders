c2d589eb82b15993bb02c660143957148947e111
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-13 18:16:47 +0300

    Laser afterglow added

---
+++ Assets/Scripts/AfterglowController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AfterglowController : MonoBehaviour {
+
+    private LineRenderer line;
+    public float fadeTime = 0.1f;
+    private float alpha = 1;
+
+
+	// Use this for initialization
+	void Start () {
+
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        line = GetComponent<LineRenderer>();
+        line.SetColors(new Color(1,0,0,alpha), new Color(1, 0, 0, alpha));
+        fadeTime -= Time.deltaTime;
+        alpha -= Time.deltaTime*10f;
+        if(fadeTime < 0.0f)
+        {
+            Destroy(gameObject);
+        }    
+	
+	}
+}
--- Assets/Scripts/LaserGunController.cs
+++ Assets/Scripts/LaserGunController.cs
@@ -2,44 +2,60 @@
 using System.Collections;
 
 public class LaserGunController : MonoBehaviour {
-	public float MaxCD = 0.2f;
+	public float MaxCD = 0.5f;
 	public int power = 0;
 	public bool isFire = false;
+    public GameObject afterglow;
 	private float CD;
 
 	private Vector3 start;
 	private Vector3 finish;
+    private Vector3 oldStart;
 
     private RaycastHit2D[] targets;
 
 	// Use this for initialization
 	void Start () {
+        start = new Vector3();
 		CD = MaxCD;
 	}
 
 
 	// Update is called once per frame
 	void Update () {
+        oldStart = start;
 		start = transform.position;
-		finish = start + new Vector3 (0, 15);
-		if (isFire) {
-			CD-=Time.deltaTime;
-			if (CD <= 0) {
-				GetComponent<LineRenderer> ().enabled = true;
-				GetComponent<LineRenderer> ().SetPosition (0, start);
-				GetComponent<LineRenderer> ().SetPosition (1, finish);
-			}
-            targets = Physics2D.RaycastAll(start, finish);
-            foreach (RaycastHit2D item in targets)
+        finish = start + new Vector3(0, 15);
+        if (isFire)
+        {
+            CD -= Time.deltaTime;
+            if (CD <= 0)
             {
-                if (item.collider.tag == "Enemy") {
-                    item.collider.SendMessage("ApplyDamage",1f+power*0.2f);
+                GetComponent<LineRenderer>().enabled = true;
+                GetComponent<LineRenderer>().SetPosition(0, start);
+                GetComponent<LineRenderer>().SetPosition(1, finish);
+                if (!start.Equals(oldStart))
+                {
+                    GameObject newGlow = Instantiate(afterglow);
+                    newGlow.GetComponent<LineRenderer>().SetPosition(0, start);
+                    newGlow.GetComponent<LineRenderer>().SetPosition(1, finish);
                 }
+                targets = Physics2D.RaycastAll(start, finish);
+                foreach (RaycastHit2D item in targets)
+                {
+                    if (item.collider.tag == "Enemy")
+                    {
+                        item.collider.SendMessage("ApplyDamage", 1f + power * 0.2f);
+                    }
 
+                }
             }
         }
         else
-			GetComponent<LineRenderer> ().enabled = false;
+        {
+            GetComponent<LineRenderer>().enabled = false;
+            CD = MaxCD;
+        }
 	}
 
 	void LevelUp(){
