a24c0c0389dd8d282a15fe07dd3871b62bec5a0d
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-15 22:15:04 +0300

    fixes for nonexistent shop demo ship

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -25,6 +25,7 @@ public class ShipController : MonoBehaviour {
 	void Start () {
 		myBody = GetComponent<Rigidbody2D> ();
 		startPosition = myBody.position;
+        if (PlayerDemoController.weaponPrefab!=null)
         Gun1 = PlayerDemoController.weaponPrefab;
         Gun1.GetComponent<AudioSource>().enabled = true;
 		Gun = (GameObject) Instantiate (Gun1,Vector3.zero, Quaternion.identity);
