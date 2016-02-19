e06a92ded9ca29c7c8d794f3e104d074be881044
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-19 05:36:17 +0300

    sliders for music and bgm

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using System.Collections;
 
 public class MusicController : MonoBehaviour {
 
     public Text infoLabel;
+    public Slider BGMSlider;
+    public Slider SNDSlider;
+    public AudioMixer mixer;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        BGMSlider.value = PlayerPrefs.GetFloat("BGM", 0);
+        SNDSlider.value = PlayerPrefs.GetFloat("SND", 0);
+        mixer.SetFloat("BGM", BGMSlider.value);
+        mixer.SetFloat("SND", SNDSlider.value);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
@@ -21,5 +29,14 @@ public class MusicController : MonoBehaviour {
         GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(fileName);
         infoLabel.SendMessage("SetMusic", "o'er the flood\r\tgoreshit");
         GetComponent<AudioSource>().Play();
+    }
+
+    public void sliderChange()
+    {
+        mixer.SetFloat("BGM",BGMSlider.value);
+        mixer.SetFloat("SND", SNDSlider.value);
+        PlayerPrefs.SetFloat("BGM", BGMSlider.value);
+        PlayerPrefs.SetFloat("SND", SNDSlider.value);
+        PlayerPrefs.Save();
     }
 }
