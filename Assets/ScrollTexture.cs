06a1e3ab2126eb33be0a83a03b8ceedb87767e9d
Andrey Kalikin <kinzo.x.beato@gmail.com>
2016-02-21 14:05:41 +0300

    nebulas for background, fixes #9
    Textures by Chris Kuznetsova

---
+++ Assets/ScrollTexture.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollTexture : MonoBehaviour {
+    public float xSpeed;
+    public float ySpeed = 0.1f;
+    private float xOffset;
+    private float yOffset;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<RawImage>().texture.wrapMode = TextureWrapMode.Repeat;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xOffset += xSpeed * Time.deltaTime;
+        yOffset += ySpeed * Time.deltaTime;
+        Rect offRect = gameObject.GetComponent<RawImage>().uvRect;
+        offRect.x = xOffset;
+        offRect.y = yOffset;
+        gameObject.GetComponent<RawImage>().uvRect = offRect;
+
+
+    }
+}
